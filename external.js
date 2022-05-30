58de7f804be526038095f9b2e6d2f3075661fb62
Ilija <ilija.kezovic4869@gmail.com>
2022-05-30 14:20:48 +0200

    Function playRound and conditions for win/lose

--- external.js
+++ external.js
@@ -6,3 +6,30 @@ function computerPlay(){
 
 let computerPoints = 0;
 let playerPoints = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return ("It's a draw!");
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        ++playerPoints
+      return ("You win! Rock beats scissors");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        ++computerPoints
+      return ("You lose! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        ++playerPoints
+      return ("You win! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        ++computerPoints
+      return ("You lose! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        ++playerPoints
+      return ("You win! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        ++computerPoints
+      return ("You lose!Rock beats scissors");
+    } else {
+      return ("Check your spelling!");
+    }
+
+}
